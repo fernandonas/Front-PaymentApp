028f9fbb0242db6dc0ae8af403e9633eaeca43a1
Fernando Augusto do Nascimento <fernando.nascimento@ambevtech.com.br>
2022-01-10 22:46:46 -0300

    w/p

--- src/app/modules/expense/components/add-expense/add-expense.component.ts
+++ src/app/modules/expense/components/add-expense/add-expense.component.ts
@@ -1,7 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-add-expense',
   templateUrl: './add-expense.component.html',
   styleUrls: ['./add-expense.component.less']
 })
-export class AddExpenseComponent { }
+
+export class AddExpenseComponent {
+  @Output() response = new EventEmitter();
+  isVisible = false;
+  addExpenseForm: FormGroup;
+
+  handleOk(): void {
+    if (this.addExpenseForm.valid) {
+      //TODO Adicionar método para adicionar expense
+    } else {
+      this.isVisible = true;
+    }
+  }
+
+  handleCancel(): void {
+    this.isVisible = false;
+  }
+
+  handleModalOpenStatus(): void {
+    this.isVisible = !this.isVisible;
+  }
+}
