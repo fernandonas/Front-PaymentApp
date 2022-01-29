ff97bad8a17ebbaa8b466fe2940a479caade5339
Fernando Augusto do Nascimento <fernando.nascimento@ambevtech.com.br>
2022-01-29 12:54:58 -0300

    Clean code add expense

--- src/app/modules/expense/components/add-expense-form/add-expense-form.component.ts
+++ src/app/modules/expense/components/add-expense-form/add-expense-form.component.ts
@@ -1,46 +1,48 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import { IPaymentInstituitionResponse } from '@interfaces/payment-instituition.interface';
 import { IPaymentTypeResponse } from '@interfaces/payment-type.interface';
 import { PaymentInstituitionService } from '@services/payment-instituition.service';
 import { PaymentTypeService } from '@services/payment-type.service';
-import { ExpenseType } from 'src/app/modules/shared/enums/expense-type.enum';
-import { PaymentStatus } from 'src/app/modules/shared/enums/payment-status.enum';
+import { ExpenseType } from '@enums/expense-type.enum';
 
 @Component({
   selector: 'app-add-expense-form',
   templateUrl: './add-expense-form.component.html',
   styleUrls: ['./add-expense-form.component.less']
 })
-export class AddExpenseFormComponent {
+export class AddExpenseFormComponent implements OnInit {
   @Output() expenseReturn = new EventEmitter();
+
   paymentInstituitions: IPaymentInstituitionResponse[];
   paymentTypes: IPaymentTypeResponse[];
   expenseForm: FormGroup;
   expenseTypeEnum = ExpenseType;
-  teste$ : any;
 
   constructor(
     private readonly formBuilder: FormBuilder,
     private readonly paymentInstituitionService: PaymentInstituitionService,
     private readonly paymentTypeService: PaymentTypeService
-  ) {
+  ) { }
+
+  ngOnInit(): void {
     this.initiateForm();
     this.getPaymentInstituition();
     this.getPaymentTypes();
   }
 
-  initiateForm(): void {
+  private initiateForm(): void {
     this.expenseForm = this.formBuilder.group({
       name: ["", Validators.required],
       purchaseDate: ["", Validators.required],
       amount: ["", Validators.required],
       expenseType: ["0", Validators.required],
-      paymentInstituitionId: [null, Validators.required],
-      paymentTypeId: [null, Validators.required],
-      paymentDate: [null, Validators.required],
+      paymentInstituitionId: [null, Validators.nullValidator],
+      paymentTypeId: [null, Validators.nullValidator],
+      paymentDate: [null, Validators.nullValidator],
       paymentStatus: ["0", Validators.required],
-      dueDate:[null, Validators.required]
+      dueDate: [null, Validators.nullValidator]
     })
   }
 
@@ -60,7 +62,7 @@ export class AddExpenseFormComponent {
     })
   }
 
-  submitForm(): void {
+  public submitForm(): void {
     this.expenseReturn.next(this.expenseForm);
   }
 }
