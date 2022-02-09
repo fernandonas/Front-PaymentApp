49f7282d11fdcee76f9e5351b7c36a0c55a1890d
Fernando Augusto do Nascimento <fernando.nascimento@ambevtech.com.br>
2022-02-08 22:31:21 -0300

    w/p

--- src/app/modules/payment-type/components/delete-payment-type/delete-payment-type.component.ts
+++ src/app/modules/payment-type/components/delete-payment-type/delete-payment-type.component.ts
@@ -1,8 +1,8 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-import { NzModalService } from 'ng-zorro-antd/modal';
+import { map } from 'rxjs/operators';
 
-import { map } from 'rxjs/internal/operators/map';
+import { NzModalService } from 'ng-zorro-antd/modal';
 
 import { IPaymentTypeResponse } from '@interfaces/payment-type.interface';
 import { PaymentTypeService } from '@services/payment-type.service';
